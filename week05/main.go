0de4dacf6ce06c287b2e98380fa2364f0d2e3587
qpa9090 <qpa9090@naver.com>
2023-09-25 11:29:04 +0900

    week05 v0.8

--- week05/main.go
+++ week05/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("input score: ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, _ := reader.ReadString('\n')
+	inputScore, err := reader.ReadString('\n') // solution option 1
+	log.Fatal(err)
 	fmt.Println(inputScore)
 }
