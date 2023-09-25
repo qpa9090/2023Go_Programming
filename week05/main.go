ce984ed69433f1e460621cbb15a891c194368c77
qpa9090 <qpa9090@naver.com>
2023-09-25 11:43:27 +0900

    week05 v0.10

--- week05/main.go
+++ week05/main.go
@@ -14,6 +14,11 @@ func main() {
 	if err != nil {                            // conditionals
 		log.Fatal(err)
 	}
+	if inputScore >= 90 { // mismatched types string and untyped int
+		grade := "A grade"
+	} else {
+		grade := "under A grade"
+	}
 
 	fmt.Println(inputScore)
 }
