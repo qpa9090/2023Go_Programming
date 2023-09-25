6a3d13f10809bd9be63c96442383b74694ba7083
qpa9090 <qpa9090@naver.com>
2023-09-25 11:53:47 +0900

    week05 v0.11

--- week05/main.go
+++ week05/main.go
@@ -5,20 +5,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("input score: ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n') // solution option 2
-	if err != nil {                            // conditionals
+	inputScore, err := reader.ReadString('\n')
+	if err != nil {
 		log.Fatal(err)
 	}
-	if inputScore >= 90 { // mismatched types string and untyped int
+	inputScore = strings.TrimSpace(inputScore)
+	score, err := strconv.ParseInt(inputScore, 32)
+
+	if inputScore >= 90 {
 		grade := "A grade"
 	} else {
 		grade := "under A grade"
 	}
 
 	fmt.Println(inputScore)
+	fmt.Println("Youu will get", grade) //undefined: grade
 }
