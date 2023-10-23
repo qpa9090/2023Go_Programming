ae04e7ced9c7f3e35a0679b45b9468ae13966085
qpa9090 <qpa9090@naver.com>
2023-10-23 11:46:28 +0900

    week09 v1.1

--- week09/main_1.go
+++ week09/main_1.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	// 포인터: 메모리 주소를 값으로 가지는 타입. & 주소연산자, *포인터 기호
 	var a int = 10
 	var pa *int
 	pa = &a
 
 	fmt.Println(&a, a)
 	fmt.Println(pa, *pa)
+	fmt.Println(&pa)
+	fmt.Print("%T %T\n", a, pa)
 }
