81f42446e50bc3801df666774792dde7ff1f5e7c
qpa9090 <qpa9090@naver.com>
2023-10-30 11:37:54 +0900

    week10 v1.1

--- week10/main.go
+++ week10/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"week10/src/greeting"
+	"week10/src/mymath"
 )
 
 func main() {
 	greeting.Hello()
 	greeting.Hi()
+	fmt.Println(mymath.MyAbs(-10))
 }
---
+++ week10/src/mymath/mymath.go
@@ -0,0 +1,8 @@
+package mymath
+
+func MyAbs(number int) int {
+	if number < 0 {
+		return number * -1
+	}
+	return number
+}
