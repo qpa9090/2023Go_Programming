fd163045b340c2dfb637fc5099d4f3c5a38e6534
qpa9090 <qpa9090@naver.com>
2023-11-06 10:56:42 +0900

    week v1.1

--- week11/main.go
+++ week11/main.go
@@ -15,5 +15,13 @@ func main() {
 
 	// 초기화 하지 않은 원소의 제로 값은 해당 원소 타입의 제로값으로 결정된다.
 	test := [5]bool{true, true, true}
-	fmt.Println(test[3])
+	fmt.Println(test[3]) // zero value
+	fmt.Println(test)
+	// fmt.print(test[5]) // comfile error, invalid argument index 5 out of bounds
+
+	i := 0
+	for i < 6 { // while
+		fmt.Println(test[i])
+		i++
+	}
 }
