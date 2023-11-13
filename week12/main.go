f49114eb28eb89b836b4961694e13af8d7abd106
qpa9090 <qpa9090@naver.com>
2023-11-13 10:44:43 +0900

    week12 v1.2

--- week12/main.go
+++ week12/main.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	// var s []int
 	// s = make([]int, 5)
-	s := make([]int, 5)
+
+	// s := make([]int, 5)
+
+	s := [5]int{0, 0, 0, 0, 0}
 
 	for _, value := range s {
 		fmt.Println(value)
