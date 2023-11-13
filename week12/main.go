8fd621b8ad546bb15c28acfcb230930ad0297599
qpa9090 <qpa9090@naver.com>
2023-11-13 10:55:52 +0900

    week12 v1.3

--- week12/main.go
+++ week12/main.go
@@ -8,9 +8,23 @@ func main() {
 
 	// s := make([]int, 5)
 
-	s := [5]int{0, 0, 0, 0, 0}
+	s := [5]int{0, 0, 0, 0, 0} // slice literal
+
+	s[4] = 99
+	s[2] = 11
+
+	// for i := 0; i < len(s); i++ {
+	// 	fmt.Println(s[i])
+	// }
 
 	for _, value := range s {
 		fmt.Println(value)
 	}
+	fmt.Println()
+
+	copyS := s[1:4]
+	for _, value := range copyS {
+		fmt.Println(value)
+	}
+
 }
