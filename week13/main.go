a68ee5a35fe73e8cc19dacd648cf617f61c1c307
qpa9090 <qpa9090@naver.com>
2023-11-20 10:43:54 +0900

    v13.1

--- week13/main.go
+++ week13/main.go
@@ -3,7 +3,10 @@ import "fmt"
 
 func main() {
 	var a []string
-	fmt.Printf("#%v\n", a)
+	var b []bool
+	fmt.Printf("#%v #%v\n", a, b)
 
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
+
 }
